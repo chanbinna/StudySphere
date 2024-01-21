a932a127401a573132ec98ebe24cfa7f8c5e2819
Chanbin Na <chanbinna@gmail.com>
2024-01-21 00:14:52 -0800

    made Home page

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import Logo from '../Logo1.svg';
 import './NavBar.css';
 
 export const NavBar = () => {
     return (
         <div className='navbar'>
+            <img className='logo' src={Logo} alt="" />
 
-
-
+            <a className='login' href="login">Log in</a>
         </div>
 
     );
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,13 +1,30 @@
+import Logo2 from '../Logo2.svg';
 import React from 'react'
+import './Home.css';
 import { NavBar } from "../components/NavBar";
+import { Footer } from '../components/Footer';
 
 
 export const Home = () => {
     return (
         <div>
             <NavBar />
-            <h1>Home Page</h1>
-            <ul>- logo</ul>
+            <div className='main'>
+                <div className='mainText'>
+                    <h1>Welcome to <br /> STUDYSPHERE</h1>
+                </div>
+                <img className='logo2' src={Logo2} alt="" />
+
+                <div className='subText'>
+                    <h2>Unlock the door to limitless learning with STUDYSPHERE:</h2>
+                    <h3>Your virtual Study Community</h3>
+                </div>
+
+            </div>
+
+
+
+            {/* <Footer /> */}
         </div>
 
     );
