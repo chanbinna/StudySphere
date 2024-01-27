7f611723fb7ff4eabbbc32b85bd33e9c84b8edd5
James Choi <jac051@ucsd.edu>
2024-01-27 10:14:28 -0800

    need debugging

--- client/src/pages/Chat.js
+++ client/src/pages/Chat.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import { NavBar } from '../components/NavBar';
+// import socket from '../utils/socket';
+import io from 'socket.io-client';
+
+const socket = io.connect("http://localhost:3001")
 
 export const Chat = () => {
+    const sendMessage = () => {
+        socket.emit("send_message", {message: "Hello"});
+    };
+    React.useEffect(() => {
+        socket.on("receive_message", (data) => {
+            alert(data.message);
+        });
+    }, [socket]);
     return (
         <div>
             <NavBar />
-            <h1>Chat</h1>
+            <input placeholder='Message...'/>
+            <button onClick={sendMessage}>Send Message</button>
         </div>
     );
 }
