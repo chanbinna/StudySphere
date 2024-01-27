805cf969557f4bf4508fae06472d7b24d8625e2d
James Choi <jac051@ucsd.edu>
2024-01-27 09:40:54 -0800

    Revert "socket.io needs debugging"
    This reverts commit 55a1e03dd2fb9b38ee371d467c03396232019d5b.

--- README.md
+++
@@ -1,8 +0,0 @@
-# StudySphere Documentation
-
-**Client Documentation**  
-[Client-Side](https://github.com/StudySphereUCSD/StudySphere/tree/main/client)  
-
-**Server Documentation**  
-[Server-Side](https://github.com/StudySphereUCSD/StudySphere/tree/main/server)  
-Remember to change config/config.json.development.password to your MySQL password
--- server/server.js
+++ server/server.js
@@ -1,27 +1,8 @@
 const express = require('express');
 const app = express();
-const http = require('http');
-const { Server } = require('socket.io')
 const cors = require('cors');
 app.use(cors());
 app.use(express.json());
-
-const server = http.createServer(app);
-
-const io = new Server(server, {
-    cors: {
-        origin: ["http://localhost:3000"],
-        methods: ["GET","POST"]
-    }
-});
-io.on("connection", (socket) => {
-    console.log(`User Connected: ${socket.id}`); // testing server connection
-    socket.on("send_message", (data) => {
-        // console.log(data);
-        socket.broadcast.emit("receive_message", data)
-    })
-});
-
 const db = require('./models');
 
 //Routers
@@ -36,6 +17,7 @@ app.use('/users', userRouter);
 app.get('/', (req, res) => {
     res.send("Main page");
 });
+
 
 db.sequelize.sync().then(() => {
     app.listen(3001, ()=> {
