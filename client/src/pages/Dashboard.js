61d86b9d26bd331b8b52f6b4c07bb66e53bfa91f
Chanbin Na <chanbinna@gmail.com>
2024-01-26 20:45:35 -0800

    dashboard

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,12 +1,48 @@
 import React from 'react';
 import { NavBar } from '../components/NavBar';
+import './Dashboard.css'
+import { SlPencil } from "react-icons/sl";
 
 
 export const Dashboard = () => {
     return (
         <div>
             <NavBar />
-            <h1>Dashboard</h1>
+            <div className='profile'>
+                <h1>Hello (Username) 👋</h1>
+                <div className='profileItem'>
+                    <div className='profilePic'>
+                        <img src="../ProfileDefault.png" alt="" />
+                    </div>
+                    <div className='profileText'>
+                        <h3>
+                            ID:<br />
+                            Name:<br />
+                            Email:<br />
+                            Grade Level:<br />
+                            Gender: <br />
+                        </h3>
+                    </div>
+                </div>
+                <div className='button-box'>
+                    <button className='edit-button'><SlPencil /> &nbsp;EDIT</button>
+                </div>
+
+
+
+            </div>
+            <div className='myGroup'>
+                <h2>My Groups</h2>
+
+            </div>
+            <div className='stat'>
+
+            </div>
+
+            <div className='another-stat'>
+
+            </div>
+
         </div>
 
     );
--- client/src/pages/FindGroup.js
+++ client/src/pages/FindGroup.js
@@ -1,6 +1,11 @@
 import React from 'react';
+import { NavBar } from '../components/NavBar';
+
 export const FindGroup = () => {
     return (
-        <h1>FindGroup</h1>
+        <div>
+            <NavBar />
+            <h1>FindGroup</h1>
+        </div>
     );
 }
