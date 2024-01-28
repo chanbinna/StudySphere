b5c06156823e00bce9ed44500c18ab737087c4c9
James Choi <jac051@ucsd.edu>
2024-01-27 16:12:47 -0800

    added chat functionality

--- server/server.js
+++ server/server.js
@@ -1,26 +1,55 @@
 const express = require('express');
 const app = express();
+const http = require('http');
+const { Server } = require('socket.io')
 const cors = require('cors');
-app.use(cors());
-app.use(express.json());
 const db = require('./models');
 
-//Routers
-const groupRouter = require('./routes/Groups');
-const chatRouter = require('./routes/Chats');
-const userRouter = require('./routes/Users');
+app.use(cors());
+app.use(express.json());
+const server = http.createServer(app);
 
-app.use('/groups', groupRouter);
-app.use('/chats', chatRouter);
-app.use('/users', userRouter);
-
-app.get('/', (req, res) => {
-    res.send("Main page");
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        // methods: ["GET", "POST"],
+    },
 });
+io.on("connection", (socket) => {
+    console.log(`User Connected: ${socket.id}`);
 
-
-db.sequelize.sync().then(() => {
-    app.listen(3001, ()=> {
-        console.log("Server running on port 3001...");
+    socket.on("join_room", (data) => {
+        socket.join(data);
+    });
+    socket.on("send_message", (data) => {
+        socket.to(data.room).emit("receive_message", data);
     });
 });
+
+const setupServer = async () => {
+    // Set up routes
+    const groupRouter = require('./routes/Groups');
+    const chatRouter = require('./routes/Chats');
+    const userRouter = require('./routes/Users');
+    app.use('/groups', groupRouter);
+    app.use('/chats', chatRouter);
+    app.use('/users', userRouter);
+
+    // Define default route
+    app.get('/', (req, res) => {
+        res.send("Main page");
+    });
+
+    // Synchronize Sequelize models with the database
+    try {
+        await db.sequelize.sync();
+        // Start the server
+        server.listen(3001, () => {
+            console.log("Server running on port 3001...");
+        });
+    } catch (error) {
+        console.error("Error synchronizing Sequelize models:", error);
+    }
+};
+
+setupServer();
